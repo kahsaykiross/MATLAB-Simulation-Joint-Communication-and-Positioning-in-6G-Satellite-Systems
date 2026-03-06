18d2872667d6d93b15b9c64377e391e48b76c746
Kahsay kiross meresa <kahsaykirossmtc@gmail.com>
2026-03-06 04:11:02 -0200

    Add initial project structure for RIS6G Simulator

---
+++ RIS6GSImulator.py
@@ -0,0 +1,40 @@
+RIS6G_SIMULATOR/
+
+main.py
+config.py
+
+environment/
+    satellite.py
+    drone.py
+    base_station.py
+    ris_surface.py
+
+channel/
+    ray_tracing.py
+    channel_model.py
+    pathloss.py
+
+communication/
+    ofdm.py
+    modulation.py
+    mimo.py
+
+positioning/
+    toa.py
+    aoa.py
+    localization.py
+
+optimization/
+    multiuser_ris_optimizer.py
+    beamforming.py
+
+ai/
+    ddpg_agent.py
+    ppo_agent.py
+    federated_learning.py
+
+dataset/
+    generator.py
+
+visualization/
+    plots.py
